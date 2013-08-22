c6d9bb5f83f0b9f0841d631c42ace6c1b1dd4a46
Micah Walter <micah@micahwalter.com>
2013-08-22 12:05:50 -0400

    uploading images

--- experts.py
+++ experts.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from flask import jsonify
+import Image
 
 from lib.shannon import *
 from lib.image_utils import *
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def experts():
-    return 'Hello Experts!'
+		# upload and store the image in memory
+		imagedata = request.files['imagedata']		
+		im = Image.open(imagedata)
+
+		# choose the method/expert
+		expert = request.form['expert']
+		
+		if expert == "shannon":
+			shannon = shannon_entropy(im)
+			
+		return jsonify(stat="ok", expert=expert, shannon=shannon)
